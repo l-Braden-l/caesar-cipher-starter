b3e872a36d4ef819afb08155c02f7040be57b71c
braden <leachbr787@northwested.org>
2024-10-04 13:06:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
     #variables
 alphabet = 'abcdefghijklmonpqrstuvwxyz'
 new_message =''
-
-user_message = input('Enter your secret message: ')
+#encrypt
+user_message = input('Enter your secret message: ').lower()
 key = int(input('Enter a key: '))
 for character in user_message:
     if character in alphabet:
@@ -14,5 +14,22 @@ for character in user_message:
         new_postion = (postion + key) % 26
         new_character = alphabet[new_postion]
         new_message += new_character
+    else:
+        new_message+= character
+print('Your new message is ' + new_message)
+#decrypt
+alphabet = 'abcdefghijklmonpqrstuvwxyz'
+new_message =''
+
+user_message = input('Enter your secret message: ').lower()
+key = int(input('Enter a key: '))
+for character in user_message:
+    if character in alphabet:
+        postion = alphabet.find(character)
+        new_postion = (postion - key) % 26
+        new_character = alphabet[new_postion]
+        new_message += new_character
+    else:
+        new_message+= character
 print('Your new message is ' + new_message)
 
